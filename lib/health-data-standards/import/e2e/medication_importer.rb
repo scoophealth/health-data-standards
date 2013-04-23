2eebeb10827a7afba1776db41c0781b864074147
Raymond Rusk <rusk.raymond@gmail.com>
2013-04-23 16:18:35 -0700

    Fully populate medication model for E2E

--- lib/health-data-standards/import/e2e/medication_importer.rb
+++ lib/health-data-standards/import/e2e/medication_importer.rb
@@ -13,23 +13,62 @@ module HealthDataStandards
       class MedicationImporter < SectionImporter
 
         def initialize
+          # start of medication section
           @entry_xpath = "//cda:section[cda:templateId/@root='2.16.840.1.113883.3.1818.10.2.19.1' and cda:code/@code='10160-6']/cda:entry/cda:substanceAdministration"
-          @timing_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:effectiveTime/cda:frequency"
-          @freetext_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship[cda:templateId/@root='2.16.840.1.113883.3.1818.10.4.35']/cda:observation/cda:text/text()"
-          @dose_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:doseQuantity"
-          @code_xpath = "./cda:consumable/cda:manufacturedProduct/cda:manufacturedLabeledDrug/cda:code"
-          @description_xpath = "./cda:consumable/cda:manufacturedProduct/cda:manufacturedLabeledDrug/cda:name/text()"
 
-          @route_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:routeCode"
-          @form_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:administrationUnitCode"
-          @status_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:statusCode"
-          #type of medication is not captured by e2e
-          #@type_of_med_xpath = "./cda:entryRelationship[@typeCode='SUBJ']/cda:observation[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.8.1']/cda:code"
-          #can't match these with e2e element
-          #@indication_xpath = "./cda:entryRelationship[@typeCode='RSON']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.1.28']/cda:code"
-          #@vehicle_xpath = "cda:participant/cda:participantRole[cda:code/@code='412307009' and cda:code/@codeSystem='2.16.840.1.113883.6.96']/cda:playingEntity/cda:code"
-          @fill_number_xpath = "./cda:entryRelationship[@typeCode='COMP']/cda:sequenceNumber/@value"
+          # location of base Entry class fields
+          @description_xpath = "./cda:consumable/cda:manufacturedProduct/cda:manufacturedLabeledDrug/cda:name/text()"
+          @entrystatus_xpath = "./cda:statusCode"            # not used
+          @code_xpath = "./cda:consumable/cda:manufacturedProduct/cda:manufacturedLabeledDrug/cda:code"
+          # use strength to populate Entry.value
           @strength_xpath = "./cda:consumable/cda:manufacturedProduct/cda:manufacturedLabeledDrug/cda:strength"
+
+          # location of Medication class fields
+          # administrationTiming [frequency of drug - could be specific time, interval (every 6 hours), duration (infuse over 30 minutes) but e2e uses frequency only]
+          @timing_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:effectiveTime/cda:frequency"
+          # freeTextSig (Instructions to patient)
+          @freetext_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship[cda:templateId/@root='2.16.840.1.113883.3.1818.10.4.35']/cda:observation/cda:text/text()"
+          # doseQuantity
+          @dose_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:doseQuantity"
+          # statusOfMedication (active, discharged, chronic, acute)
+          @status_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:statusCode"
+          # route (by mouth, intravenously, topically, etc.)
+          @route_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:routeCode"
+          # productForm (tablet, capsule, liquid, ointment)
+          @form_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:entryRelationship/cda:substanceAdministration/cda:administrationUnitCode"
+
+          # location of embedded OrderInformation class fields
+          # orderNumber (order identifier from perspective of ordering clinician)
+          @orderno_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:id"
+          # orderExpirationDateTime (Date when order is no longer valid)
+          @expiredate_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:effectiveTime/cda:high"
+          # orderDateTime (Date when order provider wrote the order/prescription)
+          @orderdate_xpath = "./cda:entryRelationship/cda:substanceAdministration/cda:author/cda:time"
+
+          # Entry fields not captured by e2e:
+          #  specifics
+          #  time
+          #  start_time
+          #  end_time
+          #  value
+          #  free_text (instructions from the ordering provider to patient)
+          #  mood_code
+
+          # Medication fields not captured by e2e:
+          #  typeOfMedication (e.g., prescription, over-counter, etc.)
+          #  site (anatomic site where medication is administered)
+          #  doseRestriction (maximum dose limit)
+          #  fulfillmentInstructions (instructions to the dispensing pharmacist)
+          #  indication (medical condition or problem addressed by the ordered product)
+          #  vehicle (substance in which the active ingredients are dispersed, e.g., saline solution)
+          #  reaction (note of intended or unintended effects, e.g., rash, nausea)
+          #  deliveryMethod (how product is administered/consumed)
+          #  patientInstructions (instructions not part of free text sig like "keep in the refrigerator")
+          #  doseIndicator (when action is to be taken, example "if blood sugar is above 250 mg/dl")
+          #
+          # OrderInformation fields not captured by e2e:
+          #  fills (number of times ordering provider has authorized pharmacy to dispense this medication)
+          #  quantityOrdered (number of dosage units or volume of liquid substance)
 
           @check_for_usable = true               # Pilot tools will set this to false
         end
@@ -40,48 +79,35 @@ module HealthDataStandards
           medication.administrationTiming={}
           medication.freeTextSig=""
           medication.dose={}
-          medication.typeOfMedication={}
+          #medication.typeOfMedication={}
           medication.statusOfMedication={}
           medication.route={}
-          medication.site={}
-          medication.doseRestriction={}
-          medication.fulfillmentInstructions=""
-          medication.indication={}
+          #medication.site={}
+          #medication.doseRestriction={}
+          #medication.fulfillmentInstructions=""
+          #medication.indication={}
           medication.productForm={}
-          medication.vehicle={}
-          medication.reaction={}
-          medication.deliveryMethod={}
-          medication.patientInstructions=""
-          medication.doseIndicator=""
+          #medication.vehicle={}
+          #medication.reaction={}
+          #medication.deliveryMethod={}
+          #medication.patientInstructions=""
+          #medication.doseIndicator=""
+          # the following isn't in the IT4 model
           #medication.cumulativeMedicationDuration={}
 
-          extract_codes(entry_element, medication)
-          #extract_dates(entry_element, medication)
           extract_description(entry_element, medication)
-          
-          #if medication.description.present?
-          #  medication.free_text = medication.description
-          #end
-          extract_free_text(entry_element, medication)
+          extract_codes(entry_element, medication)
+          extract_entry_value(entry_element, medication)
+          extract_dates(entry_element, medication)
 
           extract_administration_timing(entry_element, medication)
-
-          #medication.route = extract_code(entry_element, "./cda:routeCode")
-          #medication.dose = extract_scalar2(entry_element, "./cda:strength/cda:center")
-          #medication.site = extract_code(entry_element, "./cda:approachSiteCode", 'SNOMED-CT')
+          extract_freetextsig(entry_element, medication)
+          extract_dose(entry_element, medication)
+          extract_status(entry_element, medication)
           extract_route(entry_element, medication)
           extract_form(entry_element, medication)
-          extract_dose_restriction(entry_element, medication)
-          extract_dose(entry_element, medication)
-
-          #medication.product_form = extract_code(entry_element, "./cda:administrationUnitCode", 'NCI Thesaurus')
-          #medication.delivery_method = extract_code(entry_element, "./cda:code", 'SNOMED-CT')
-          #medication.type_of_medication = extract_code(entry_element, @type_of_med_xpath, 'SNOMED-CT') if @type_of_med_xpath
-          #medication.indication = extract_code(entry_element, @indication_xpath, 'SNOMED-CT')
-          #medication.vehicle = extract_code(entry_element, @vehicle_xpath, 'SNOMED-CT')
 
           extract_order_information(entry_element, medication)
-          extract_dates(entry_element, medication)
           extract_author_time(entry_element, medication)
           extract_fulfillment_history(entry_element, medication)
           medication
@@ -96,9 +122,14 @@ module HealthDataStandards
           end
         end
 
-        # Find date in Medication Prescription Event. Commented out because there should be an
-        # orderinformation object created to hold the prescription dates.
+        def extract_entry_value(parent_element, entry)
+          value = {}
+          value['value'] = parent_element.xpath(@strength_xpath+"/cda:center/@value").to_s
+          value['unit'] = parent_element.xpath(@strength_xpath+"/cda:center/@unit").to_s
+          entry.value = value
+        end
 
+        # Find date in Medication Prescription Event.
         def extract_dates(parent_element, entry, element_name="effectiveTime")
           #print "XML Node: " + parent_element.to_s + "\n"
           if parent_element.at_xpath("cda:entryRelationship/cda:substanceAdministration/cda:#{element_name}")
@@ -117,6 +148,80 @@ module HealthDataStandards
           #print "Time: " + entry.time.to_s + "\n"
           #print "Start Time: " + entry.start_time.to_s + "\n"
           #print "End Time: " + entry.end_time.to_s + "\n"
+        end
+
+        # only handles drug administration timing expressed as a frequency
+        # (does not handle specific time, interval or duration specifications)
+        def extract_administration_timing(parent_element, medication)
+          ate = parent_element.xpath(@timing_xpath)
+          #print "administration_timing: " + ate.to_s + "\n"
+          if ate
+            at = {}
+            at['numerator'] = extract_scalar(ate, "./cda:numerator")
+            at['denominator'] = extract_scalar(ate, "./cda:denominator")
+            medication.administration_timing['frequency'] = at
+          end
+        end
+
+        def extract_freetextsig(parent_element, entry)
+          entry.freeTextSig = parent_element.xpath(@freetext_xpath).to_s
+        end
+
+        def extract_dose(parent_element, entry)
+          dose = {}
+          dose['low'] = parent_element.xpath(@dose_xpath+'/cda:low/@value').to_s
+          dose['high'] = parent_element.xpath(@dose_xpath+"/cda:high/@value").to_s
+          entry.dose = dose
+        end
+
+        def extract_status(parent_element, entry)
+          status_element = parent_element.xpath(@status_xpath+"/@code").to_s
+          #print "status: " + status_element.to_s + "\n"
+          entry.statusOfMedication = status_element
+        end
+
+        def extract_route(parent_element, entry)
+          code_elements = parent_element.xpath(@route_xpath)
+          #print "Route XML Node: " + code_elements.to_s + "\n"
+          code_elements.each do |code_element|
+            route = {}
+            route['code'] = code_element['code']
+            route['codeSystem'] = code_element['codeSystem']
+            route['codeSystemName'] = code_element['codeSystemName']
+            route['displayName'] = code_element['displayName']
+            entry.route = route
+          end
+        end
+
+        def extract_form(parent_element, entry)
+          code_elements = parent_element.xpath(@form_xpath)
+          #print "Form XML Node: " + code_elements.to_s + "\n"
+          code_elements.each do |code_element|
+            content = {}
+            content['code'] = code_element['code']
+            content['codeSystem'] = code_element['codeSystem']
+            content['displayName'] = code_element['displayName']
+            entry.productForm = content
+          end
+          #print "code: " + entry.productForm.to_s + "\n"
+        end
+
+        def extract_order_information(parent_element, medication)
+          order_elements = parent_element.xpath("./cda:entryRelationship[@typeCode='REFR']/cda:supply[@moodCode='INT']")
+          if order_elements
+            order_elements.each do |order_element|
+              order_information = OrderInformation.new
+              actor_element = order_element.at_xpath('./cda:author')
+              if actor_element
+                order_information.provider = ProviderImporter.instance.extract_provider(actor_element, "assignedAuthor")
+              end
+              order_information.order_number = order_element.at_xpath('./cda:id').try(:[], 'root')
+              order_information.fills = order_element.at_xpath('./cda:repeatNumber').try(:[], 'value').try(:to_i)
+              order_information.quantity_ordered = extract_scalar(order_element, "./cda:quantity")
+
+              medication.orderInformation << order_information
+            end
+          end
         end
 
         def extract_author_time(parent_element, entry, element_name="author")
@@ -146,23 +251,7 @@ module HealthDataStandards
           end
         end
 
-        def extract_order_information(parent_element, medication)
-          order_elements = parent_element.xpath("./cda:entryRelationship[@typeCode='REFR']/cda:supply[@moodCode='INT']")
-          if order_elements
-            order_elements.each do |order_element|
-              order_information = OrderInformation.new
-              actor_element = order_element.at_xpath('./cda:author')
-              if actor_element
-                order_information.provider = ProviderImporter.instance.extract_provider(actor_element, "assignedAuthor")
-              end
-              order_information.order_number = order_element.at_xpath('./cda:id').try(:[], 'root')
-              order_information.fills = order_element.at_xpath('./cda:repeatNumber').try(:[], 'value').try(:to_i)
-              order_information.quantity_ordered = extract_scalar(order_element, "./cda:quantity")
-          
-              medication.orderInformation << order_information
-            end
-          end
-        end
+
 
         #def extract_order_information(parent_element, medication)
         #  order_elements = parent_element.xpath("./cda:entryRelationship[@typeCode='REFR']/cda:supply[@moodCode='INT']")
@@ -179,105 +268,16 @@ module HealthDataStandards
         #  end
         #end
 
-        #def extract_administration_timing(parent_element, medication)
-        #  administration_timing_element = parent_element.at_xpath("./cda:effectiveTime[2]")
-        #  if administration_timing_element
-        #    at = {}
-        #    if administration_timing_element['institutionSpecified']
-        #      at['institutionSpecified'] = administration_timing_element['institutionSpecified'].to_boolean
-        #    end
-        #    at['period'] = extract_scalar(administration_timing_element, "./cda:period")
-        #    if at.present?
-        #      medication.administration_timing = at
-        #    end
+
+        #def extract_dose_restriction(parent_element, medication)
+        #  dre = parent_element.at_xpath("./cda:maxDoseQuantity")
+        #  if dre
+        #    dr = {}
+        #    dr['numerator'] = extract_scalar(dre, "./cda:numerator")
+        #    dr['denominator'] = extract_scalar(dre, "./cda:denominator")
+        #    medication.dose_restriction = dr
         #  end
         #end
-
-        def extract_administration_timing(parent_element, medication)
-          ate = parent_element.xpath(@timing_xpath)
-          #print "administration_timing: " + ate.to_s + "\n"
-          if ate
-            at = {}
-            at['numerator'] = extract_scalar(ate, "./cda:numerator")
-            at['denominator'] = extract_scalar(ate, "./cda:denominator")
-            medication.administration_timing = at
-          end
-        end
-
-        def extract_free_text(parent_element, entry)
-          code_elements = parent_element.xpath(@freetext_xpath)
-          #print "free text: " + code_elements.to_s + "\n"
-          code_elements.each do |code_element|
-            entry.free_text = code_element
-          end
-        end
-
-        def extract_dose(parent_element, entry)
-          code_elements = parent_element.xpath(@dose_xpath)
-          print "XML Node: " + code_elements.to_s + "\n"
-          code_elements.each do |code_element|
-            drs = code_element
-            #print "drs: " + drs.to_s + "\n"
-            if drs
-              dr = {}
-              dr['low'] = drs['low']
-              dr['high'] = drs['high']
-              entry.dose = dr
-              print "dose: " + entry.dose.to_s + "\n"
-            end
-          end
-        end
-
-        def extract_dose_restriction(parent_element, medication)
-          dre = parent_element.at_xpath("./cda:maxDoseQuantity")
-          if dre
-            dr = {}
-            dr['numerator'] = extract_scalar(dre, "./cda:numerator")
-            dr['denominator'] = extract_scalar(dre, "./cda:denominator")
-            medication.dose_restriction = dr
-          end
-        end
-
-        def extract_description(parent_element, entry)
-          code_elements = parent_element.xpath(@description_xpath)
-          code_elements.each do |code_element|
-            entry.description = code_element
-          end
-        end
-
-      def extract_status(parent_element, entry)
-        code_elements = parent_element.xpath(@status_xpath)
-        code_elements.each do |code_element|
-          entry.status = code_element
-        end
-      end
-
-
-      def extract_route(parent_element, entry)
-          code_elements = parent_element.xpath(@route_xpath)
-          #print "Route XML Node: " + code_elements.to_s + "\n"
-          code_elements.each do |code_element|
-            route = {}
-            route['code'] = code_element['code']
-            route['codeSystem'] = code_element['codeSystem']
-            route['codeSystemName'] = code_element['codeSystemName']
-            route['displayName'] = code_element['displayName']
-            entry.route = route
-          end
-        end
-
-        def extract_form(parent_element, entry)
-          code_elements = parent_element.xpath(@form_xpath)
-          #print "Form XML Node: " + code_elements.to_s + "\n"
-          code_elements.each do |code_element|
-            content = {}
-            content['code'] = code_element['code']
-            content['codeSystem'] = code_element['codeSystem']
-            content['displayName'] = code_element['displayName']
-            entry.productForm = content
-          end
-          #print "code: " + entry.productForm.to_s + "\n"
-        end
 
 
 
