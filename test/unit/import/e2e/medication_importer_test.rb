b131169f286829875fe5cb0b0d3b579da516c745
Raymond Rusk <rusk.raymond@gmail.com>
2013-04-24 11:33:06 -0700

    E2E medication importer for IT4

--- test/unit/import/e2e/medication_importer_test.rb
+++ test/unit/import/e2e/medication_importer_test.rb
@@ -10,35 +10,74 @@ class MedicationImporterTest < MiniTest::Unit::TestCase
     patient = pi.parse_e2e(doc)
 
     patient.save!
-    
+
+    # first listed medication
     medication = patient.medications[0]
+    assert_equal "TYLENOL EXTRA STRENGTH TAB 500MG", medication.description
+
     assert medication.codes['HC-DIN'].include? '00559407'
     assert medication.codes['whoATC'].include? 'N02BE01'
 
- #   assert_equal 6, medication.administration_timing['period']['value']
- #   assert_equal 'IPINHL', medication.route['code']
- #   assert_equal 2, medication.dose['value']
- #   assert_equal '127945006', medication.site['code']
- #   assert_equal 'SNOMED-CT', medication.site['codeSystem']
- #   assert_equal 5, medication.dose_restriction['numerator']['value']
- #   assert_equal 10, medication.dose_restriction['denominator']['value']
- #   assert_equal '415215001', medication.product_form['code']
- #   assert_equal '334980009', medication.delivery_method['code']
- #   assert_equal '73639000', medication.type_of_medication['code']
- #   assert_equal 'DrugVehicleCode', medication.vehicle['code']
- #   assert_equal 'eleventeen', medication.fulfillment_history[0].prescription_number
- #   assert_equal 'Pseudo', medication.fulfillment_history[0].provider.given_name
- #   assert_equal '100 Bureau Drive', medication.fulfillment_history[0].provider.addresses[0].street[0]
- #   assert_equal 1316476800, medication.fulfillment_history[0].dispense_date
- #   assert_equal 30, medication.fulfillment_history[0].quantity_dispensed['value']
- #   assert_equal 4, medication.fulfillment_history[0].fill_number
+    assert_equal "500.0", medication.value['value']
+    assert_equal "MG", medication.value['unit']
 
- #   medication = patient.medications[3]
- #   assert_equal 'VA/KPproblemList', medication.indication['code']
- #   assert_equal 1, medication.order_information.size
- #   assert_equal 1, medication.order_information.first.fills
- #   assert_equal 1, medication.order_information.first.quantity_ordered['value']
- #   assert_equal 'tablet', medication.order_information.first.quantity_ordered['unit']
+    assert_equal 1362441600, medication.time
+    assert_equal 1362441600, medication.start_time
+    assert_equal 1366761600, medication.end_time
 
+    assert_equal 4, medication.administration_timing['frequency']['numerator']['value']
+    assert_equal 1, medication.administration_timing['frequency']['denominator']['value']
+    assert_equal 'd', medication.administration_timing['frequency']['denominator']['unit']
+
+    assert_equal 'Special medication instructions to patient', medication.freeTextSig
+
+    assert_equal '1.0', medication.dose['low']
+    assert_equal '2.0', medication.dose['high']
+
+    assert_equal 'active', medication.statusOfMedication
+
+    assert_equal 'PO', medication.route['code']
+    assert_equal '2.16.840.1.113883.5.112', medication.route['codeSystem']
+    assert_equal 'RouteOfAdministration', medication.route['codeSystemName']
+    assert_equal 'PO', medication.route['displayName']
+
+    assert_equal 'TAB', medication.product_form['code']
+    assert_equal '2.16.840.1.113883.5.1127', medication.product_form['codeSystem']
+    assert_equal 'TABLET', medication.product_form['displayName']
+
+
+    # last listed medication
+    medication = patient.medications[8]
+    assert_equal "ATORVASTATIN 40MG", medication.description
+
+    assert medication.codes['HC-DIN'].include? '02387913'
+    assert medication.codes['whoATC'].include? 'C10AA05'
+
+    assert_equal "40.0", medication.value['value']
+    assert_equal "MG", medication.value['unit']
+
+    assert_equal 1362441600, medication.time   # returns nil?
+    assert_equal 1362441600, medication.start_time
+    assert_equal 1367280000, medication.end_time
+
+    assert_equal 1, medication.administration_timing['frequency']['numerator']['value']
+    assert_equal 1, medication.administration_timing['frequency']['denominator']['value']
+    assert_equal 'd', medication.administration_timing['frequency']['denominator']['unit']
+
+    assert_equal '', medication.freeTextSig
+
+    assert_equal '1.0', medication.dose['low']
+    assert_equal '1.0', medication.dose['high']
+
+    assert_equal 'active', medication.statusOfMedication
+
+    assert_equal 'PO', medication.route['code']
+    assert_equal '2.16.840.1.113883.5.112', medication.route['codeSystem']
+    assert_equal 'RouteOfAdministration', medication.route['codeSystemName']
+    assert_equal 'PO', medication.route['displayName']
+
+    assert_equal 'TAB', medication.product_form['code']
+    assert_equal '2.16.840.1.113883.5.1127', medication.product_form['codeSystem']
+    assert_equal 'TABLET', medication.product_form['displayName']
   end
 end
