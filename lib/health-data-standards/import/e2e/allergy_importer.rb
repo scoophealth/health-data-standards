971d612a5440ad466673dcd7e341fc2fc7b83670
Raymond Rusk <rusk.raymond@gmail.com>
2013-08-09 15:41:53 -0700

    Complete allergy import and unit tests for new PITO E2E complete example

--- lib/health-data-standards/import/e2e/allergy_importer.rb
+++ lib/health-data-standards/import/e2e/allergy_importer.rb
@@ -1,17 +1,37 @@
+# XPath prefix: /ClinicalDocument/component/structuredBody/component/section/entry/act
+#
+# OSCAR Field          Business Term              Field               XPath (suffix)
+# -----------          -------------              -----               --------------
+# allergyid            Record ID                  id extension        ./id/@extension
+# archived             Allergy/Intolerance Status statusCode          ./statusCode/@code
+# lastUpdateDate       Documented Date            effectiveTime value ./effectiveTime/@value
+# start_date           Onset Date & Resolved Date effectiveTime >     ./entryRelationship/observation/effectiveTime/low/@value
+#                                                      low value      ./entryRelationship/observation/entryRelationship/observation/effectiveTime/low/@value
+# TYPECODE             Adverse Event Type Code    code                ./entryRelationship/observation[participant]/code/@code
+# regional_identifier  Coded Allergen             code                ./entryRelationship/observation/participant/participantRole/playingEntity/code/@code
+# DESCRIPTION          Free Text Allergen         name                ./entryRelationship/observation/participant/participantRole/playingEntity/name
+# reaction             Reaction Name              text                ./entryRelationship/observation/entryRelationship/observation[code/@code='REACTOBS']/text
+# severity_of_reaction Coded Severity Value       value code          ./entryRelationship/observation/entryRelationship/observation/value/@code
+#                                                                     ./entryRelationship/observation/entryRelationship/observation/entryRelationship/observation/value/@code
+# entry_date           Authored Date/Time         time value          ./entryRelationship/observation/entryRelationship/observation/author/time/@value
+# life_stage           Lifestage at Onset         entryRelationship   ./entryRelationship/observation/entryRelationship/observation/value/@code
+# providerNo           Reaction Author            assignedPerson      ./entryRelationship/observation/entryRelationship/observation/author/assignedAuthor/assignedPerson/name
+#                                                 > name > given,family
+
 module HealthDataStandards
   module Import
     module E2E
       class AllergyImporter < SectionImporter
 
         def initialize
-          @entry_xpath = "//cda:section[cda:templateId/@root='2.16.840.1.113883.3.1818.10.2.4.1']/cda:entry/cda:act"
+          #@entry_xpath = "//cda:section[cda:templateId/@root='2.16.840.1.113883.3.1818.10.2.4.1']/cda:entry/cda:act"
+          @entry_xpath = "/cda:ClinicalDocument/cda:component/cda:structuredBody/cda:component/cda:section/cda:entry/cda:act"
           @code_xpath = "./cda:entryRelationship/cda:observation/cda:participant/cda:participantRole/cda:playingEntity/cda:code"
           @description_xpath = "./cda:entryRelationship/cda:observation/cda:participant/cda:participantRole/cda:playingEntity/cda:name"
-          @type_xpath = "./cda:code"
-          #No support for reaction type (i.e., 'hives', etc.) in OSCAR E2E
-          #@reaction_xpath = "./cda:entryRelationship[@typeCode='MFST']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.1.54']/cda:value"
-          @reaction_xpath = "./cda:entryRelationship/cda:observation/cda:entryRelationship/cda:observation[cda:code/@code='REACTOBS']/cda:text"
-          @severity_xpath = "./cda:entryRelationship/cda:observation/cda:entryRelationship[cda:templateId/@root='2.16.840.1.113883.3.1818.10.4.30']/cda:observation/cda:value"
+          @type_xpath = "./cda:entryRelationship/cda:observation[cda:participant]/cda:code"
+          @reaction_xpath = "./cda:entryRelationship/cda:observation/cda:entryRelationship/cda:observation[cda:code/@code='REACTOBS']"
+          #@severity_xpath = "./cda:entryRelationship/cda:observation/cda:entryRelationship[cda:templateId/@root='2.16.840.1.113883.3.1818.10.4.30']/cda:observation/cda:value"
+          @severity_xpath = "./ cda:entryRelationship/cda:observation/cda:entryRelationship/cda:observation[cda:code/@code='SEV']/cda:value"
           @status_xpath   = "./cda:entryRelationship/cda:observation/cda:entryRelationship/cda:observation/cda:value[@codeSystem='2.16.840.1.113883.3.1818.10.2.8.2']"
           @id_map = {}
         end
@@ -27,7 +47,6 @@ module HealthDataStandards
           allergy_list = []
           entry_elements = doc.xpath(@entry_xpath)
           entry_elements.each do |entry_element|
-            #STDERR.puts "ENTRY_ELEMENT: " + entry_element.inspect
             allergy = Allergy.new
             extract_e2e_codes(entry_element, allergy)
             extract_dates(entry_element, allergy)
@@ -35,14 +54,10 @@ module HealthDataStandards
             extract_negation(entry_element, allergy)
 
             extract_e2e_status(entry_element, allergy)
-            allergy.type = extract_code(entry_element, @type_xpath)
-            allergy.reaction = extract_code(entry_element, @reaction_xpath)
-            #STDERR.puts "reaction: "+allergy.reaction.inspect
-            allergy.severity = extract_code(entry_element, @severity_xpath)
-            #STDERR.puts "ENTRY_ELEMENT: " +entry_element
-            #STDERR.puts "SEVERITY_XPATH: " +@severity_xpath
-            #STDERR.puts "ALLERGY.SEVERITY: "+allergy.severity.inspect
-            #puts "ALLERGY: " + allergy.inspect
+            allergy.type = extract_e2e_type(entry_element, allergy)
+
+            allergy.reaction = extract_e2e_reaction(entry_element, allergy)
+            allergy.severity = extract_e2e_severity(entry_element, allergy)
             allergy_list << allergy
           end
           allergy_list
@@ -62,8 +77,6 @@ module HealthDataStandards
             entry.add_code(code_element['code'], CodeSystemHelper.code_system_for(code_element['codeSystem']))
           elsif code_element['nullFlavor']
             entry.add_code(code_element['nullFlavor'], 'Unknown')
-          else
-            STDERR.puts "CODE_ELEMENT: " + code_element.inspect
           end
         end
 
@@ -76,8 +89,64 @@ module HealthDataStandards
           status_element = parent_element.at_xpath(@status_xpath)
           if status_element
             entry.status_code = {CodeSystemHelper.code_system_for(status_element['codeSystem']) => [status_element['code']]}
-            #entry.status = {CodeSystemHelper.code_system_for(status_element['codeSystem']) => [status_element['code']]}
           end
+          entry.status_code
+        end
+
+        def extract_e2e_type(parent_element, entry)
+          type_element = parent_element.xpath(@type_xpath)
+          entry.type = nil
+          if type_element
+            element = {}
+            element['code'] = parent_element.xpath(@type_xpath+"/@code").to_s
+            element['displayName'] = parent_element.xpath(@type_xpath+"/@displayName").to_s
+            element['codeSystem'] = parent_element.xpath(@type_xpath+"/@codeSystem").to_s
+            element['codeSystemName'] = parent_element.xpath(@type_xpath+"/@codeSystemName").to_s
+            if element['code'] != ""
+              entry.type = element
+            end
+          end
+          entry.type
+        end
+
+        def extract_e2e_severity(parent_element, entry)
+          severity_element = parent_element.xpath(@severity_xpath)
+          if severity_element
+            element = {}
+            element['code'] = parent_element.xpath(@severity_xpath+"/@code").to_s
+            element['displayName'] = parent_element.xpath(@severity_xpath+"/@displayName").to_s
+            element['codeSystem'] = parent_element.xpath(@severity_xpath+"/@codeSystem").to_s
+            element['codeSystemName'] = parent_element.xpath(@severity_xpath+"/@codeSystemName").to_s
+            if element['code'] != ""
+              entry.severity = element
+            else
+              entry.severity = nil
+            end
+          end
+          entry.severity
+        end
+
+        def extract_e2e_reaction(parent_element, entry)
+          entry.reaction = {}
+          reaction_text = parent_element.xpath(@reaction_xpath+"/cda:text")
+          if reaction_text
+            entry.reaction["text"] = reaction_text.text()
+          end
+          reaction_value = parent_element.xpath(@reaction_xpath+"/cda:value")
+          if reaction_value
+            element = {}
+            element["code"] = parent_element.xpath(@reaction_xpath+"/cda:value/@code").to_s
+            element["displayName"] = parent_element.xpath(@reaction_xpath+"/cda:value/@displayName").to_s
+            element["codeSystem"] = parent_element.xpath(@reaction_xpath+"/cda:value/@codeSystem").to_s
+            element["codeSystemName"] = parent_element.xpath(@reaction_xpath+"/cda:value/@codeSystemName").to_s
+            if element["code"] != ""
+              entry.reaction["value"] = element
+            else
+              entry.reaction["value"] = nil
+            end
+          end
+          entry.reaction
+
         end
 
       end
--- test/unit/import/e2e/allergy_importer_test.rb
+++ test/unit/import/e2e/allergy_importer_test.rb
@@ -11,10 +11,14 @@ class AllergyImporterTest < MiniTest::Unit::TestCase
     allergy = patient.allergies[0]
     assert_equal false, allergy.nil?
     assert_equal 'PENICILLINS, COMBINATIONS WITH OTHER ANTIBACTERIAL', allergy.description
-    #assert_equal 'MI', allergy.severity['code']
-    #John Cleese no longer has a valid code after Jeremy's July 31, 2013 update.
-    assert_equal nil, allergy.severity['code']
-    assert_equal nil, allergy.reaction
+    assert_equal allergy.codes, {"Unknown"=>["NA"]}
+    assert_equal "MED", allergy.type['code']
+    assert_equal "Medication", allergy.type['displayName']
+    assert_equal "2.16.840.1.113883.5.4", allergy.type['codeSystem']
+    assert_equal "HL7 ActCode", allergy.type['codeSystemName']
+    assert_equal "", allergy.reaction['text']
+    assert_equal nil, allergy.reaction['value']
+    assert_equal nil, allergy.severity
     assert_equal 'C', allergy.status_code['PITO AlleryClinicalStatus'][0]
     assert_equal Time.gm(2013,3,5).to_i, allergy.time
 
@@ -33,8 +37,21 @@ class AllergyImporterTest < MiniTest::Unit::TestCase
     allergy = patient.allergies[0]
     assert_equal false, allergy.nil?
     assert_equal 'Peanut allergy', allergy.description
+    assert_equal allergy.codes, {"SNOMED-CT"=>["256349002"]}
+    assert_equal "FALG", allergy.type['code']
+    assert_equal "Food Allergy", allergy.type['displayName']
+    assert_equal "2.16.840.1.113883.5.4", allergy.type['codeSystem']
+    assert_equal "HL7 ActCode", allergy.type['codeSystemName']
     assert_equal 'A4', allergy.severity['code']
-    assert_equal nil, allergy.reaction['code']
+    assert_equal 'Severe', allergy.severity['displayName']
+    assert_equal '2.16.840.1.113883.5.1063', allergy.severity['codeSystem']
+    assert_equal 'HL7 ObservationValue', allergy.severity['codeSystemName']
+    assert_equal 'Severe reaction to peanuts', allergy.reaction['text']
+    assert_equal '417516000', allergy.reaction['value']['code']
+    assert_equal 'Anaphylaxis due to substance', allergy.reaction['value']['displayName']
+    assert_equal '2.16.840.1.113883.6.96', allergy.reaction['value']['codeSystem']
+    assert_equal 'SNOMED CT', allergy.reaction['value']['codeSystemName']
+
     assert_equal 'C', allergy.status_code['PITO AlleryClinicalStatus'][0]
     assert_equal Time.gm(2011,2,14).to_i, allergy.time
 
